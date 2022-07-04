cbe08aeff77ac04963a3902d916f11b8094845c0
KarthikTamilan <99072674+KarthikTamilan@users.noreply.github.com>
2022-07-04 18:01:59 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,10 @@
 class Presets(object):
     WELCOME_MESSAGE = """
 <b>Hello.. </b>{}
-<code>I'm an HMTD Official File Store Bot 
-Maintained by</code> <b>{}</b>
-<code>Keep me Join to Our Official Channel to receive Bot & Movies updates in @HMTD_Links.</code>
+<b>I'm an HMTD Official File Store Bot 
+Maintained by</b> <b>{}</b>
+<b>Keep me Join to Our Official Channel to receive Bot & Movies updates in @HMTD_Links.</b>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
-    REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
+    REPLY_ERROR = "<b>Use this command as a reply to any telegram message with out any spaces.</b>"
