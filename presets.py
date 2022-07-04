39773f37c6510855d57b64e4db9c1134f209f4d8
KarthikTamilan <99072674+KarthikTamilan@users.noreply.github.com>
2022-07-04 18:05:07 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,9 @@
 class Presets(object):
     WELCOME_MESSAGE = """
-<b>Hello.. </b>{}
+<b>Hello... </b>{}
 <b>I'm an HMTD Official File Store Bot 
-Maintained by</b> <b>{}</b>
-<b>Keep me Join to Our Official Channel to receive Bot & Movies updates in @HMTD_Links.</b>
+Maintained by</b> <b>{}</b>.
+<b>Keep me Join to Our Official Channel to Receive Bot & Movies Updates in @HMTD_Links.</b>
     """
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
